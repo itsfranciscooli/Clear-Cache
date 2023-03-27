aa084c42e096d82ca5f769eb277ecc248f0f3621
Francisco Oliveira <75614393+itsfranciscooli@users.noreply.github.com>
2023-03-28 00:54:54 +0100

    Logs Added

--- cache-clear.php
+++ cache-clear.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Clear Cache
  * Description: This plugin clears the cache whenever a new post is published or updated.
- * Version: 1.0
+ * Version: 1.1
  * Author: Francisco Oliveira
  * Author URI: https://franciscooliveira.me/
  */
@@ -13,5 +13,17 @@ function clear_cache_on_publish_or_update($post_id) {
     }
 
     wp_cache_flush();
+    update_option('last_cache_flush', current_time('mysql'));
 }
 add_action('save_post', 'clear_cache_on_publish_or_update');
+
+function add_last_cache_flush_section() {
+    $last_flush_time = get_option('last_cache_flush');
+    $last_flush_time_formatted = date('Y-m-d H:i:s', strtotime($last_flush_time));
+    ?>
+    <div class="notice notice-info">
+        <p><?php printf(__('The cache was last flushed on %s', 'my-plugin'), $last_flush_time_formatted); ?></p>
+    </div>
+    <?php
+}
+add_action('admin_notices', 'add_last_cache_flush_section');
